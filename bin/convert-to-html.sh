1d35fc79780f425eaa7f44b3d5f7736ac91ee1d8
dacendi <sbazaud@dacendi.net>
2016-10-14 17:46:52 +0200

    Correction de l'export en format texte

--- bin/convert-to-html.sh
+++ bin/convert-to-html.sh
@@ -12,5 +12,5 @@ for dir in $DIRS; do
 	mkdir -p $HTML/$dir $TXT/$dir
 	rm $HTML/$dir/*.html
 	soffice --headless --convert-to html --outdir $HTML/$dir $SOURCES/$dir/*.doc $SOURCES/$dir/*.docx $SOURCES/$dir/*.odt
-        soffice --headless --convert-to txt --outdir $TXT/$dir $SOURCES/$dir/*.doc $SOURCES/$dir/*.docx $SOURCES/$dir/*.odt
+        soffice --headless --convert-to "txt:Text (encoded):UTF8" --outdir $TXT/$dir $SOURCES/$dir/*.doc $SOURCES/$dir/*.docx $SOURCES/$dir/*.odt
 done
